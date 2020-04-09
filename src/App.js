90802941840a3e2fa1d38bb1e22b0a4b875f4362
Arnaud LEBRETON <arnolebreton@gmail.com>
2020-04-09 12:28:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="primary">Primary</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
